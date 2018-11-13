290e39f0a472b2a8150f8d9adaa412d9aa9424ad
Aaliyah Fentress <fentressaaliyah@gmail.com>
2018-11-13 22:57:38 +0000

    rps

--- js/script.js
+++ js/script.js
@@ -6,13 +6,29 @@
 /* global $ */
 
 var computerChoice;
+computerChoice="";
 var winner; 
-var randomNumber;
-randomNumber=0;
+winner="";
+var userChoice;
+userChoice="";
+var randomNumber=Math.random();
+
 
 
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function(){
-    $("#userChoice").text($("#input").val());
+    userChoice=$("#input").val();
+    $("#userChoice").text(userChoice);
     
 });
+console.log(randomNumber);
+    if(randomNumber < .10) {
+        $("#computerChoice").html("Rock"); 
+    } else if (randomNumber > .20) {
+        $("#computerChoice").html("Paper");
+    } else if (randomNumber < .40) {
+        $("#computerChoice").html("Scissor");
+    }
+    
+ 
+
